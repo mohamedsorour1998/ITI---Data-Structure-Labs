5166a87aebe6cd6b43b0e3b76349f44261dccc49
Mohamed Sorour <110028481+mohamedsorour1998@users.noreply.github.com>
2022-12-24 11:07:13 +0200

    Added LAB1 & LAB2

---
+++ Lab2 DS/LAB2/LAB2/main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "STACK IN LINKED LIST.h"
+#include "QUEUE IN LINKED LIST.h"
+
+int main()
+{
+
+Push(3);
+Push(5);
+Push(7);
+Push(9);
+
+Pop();
+
+    display();/*
+
+Stack stk ={.top = -1};
+
+push(&stk, 10);
+push(&stk, 20);
+push(&stk, 30);
+
+int num;
+
+if (Pop(&stk, &num))
+{
+    //pop true
+}
+else {
+        //pop false
+
+}
+queue que ={.front=-1, .rear=-1};
+EnQueue (&que, 10);
+EnQueue (&que, 20);
+EnQueue (&que, 30);
+
+if (DeQueue(&que, &num))
+{
+    //pop true
+}
+else {
+        //pop false
+
+}*/
+    return 0;
+}
